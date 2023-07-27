c45fc71292f663f51dc265bdbefdcc3cc588d072
Abhay Mathur <66543532+abhayma1000@users.noreply.github.com>
2023-07-27 09:26:55 -0400

    Added stretches

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	stretchesBaseUrl := "https://musclewiki.com/stretches/male"
 	baseUrl := "https://musclewiki.com/exercises/male"
 
 	workouts := []string{"chest", "shoulders", "traps", "biceps"}
@@ -16,6 +17,10 @@ func main() {
 	workoutHandler := func(w http.ResponseWriter, req *http.Request) {
 
 		num := rand.Intn(len(workouts))
+
+		io.WriteString(w, fmt.Sprintf("%s/%s", stretchesBaseUrl, workouts[num]))
+
+		io.WriteString(w, "\n")
 
 		io.WriteString(w, fmt.Sprintf("%s/%s", baseUrl, workouts[num]))
 	}
